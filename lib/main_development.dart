c305715e55ac4b890aec0c6b1464c2778ff6b976
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2024-10-10 12:44:02 +0700

    fix powersync web

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,8 +1,15 @@
 import 'package:api_repository/api_repository.dart';
 import 'package:conexion/app/app.dart';
 import 'package:conexion/bootstrap.dart';
+import 'package:conexion/firebase_options_dev.dart';
+import 'package:shared/shared.dart';
 
 void main() {
+  // ignore: lines_longer_than_80_chars
   const apiRepository = ApiRepository();
-  bootstrap(() => const App(apiRepository: apiRepository,));
+  bootstrap((powerSyncRepository) {
+    return const App(apiRepository: apiRepository);
+  }, 
+  appFlavor: AppFlavor.development(),
+  firebaseOptions: DefaultFirebaseOptions.currentPlatform,);
 }
--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,10 +1,18 @@
 import 'package:api_repository/api_repository.dart';
 import 'package:conexion/app/app.dart';
 import 'package:conexion/bootstrap.dart';
+import 'package:conexion/firebase_options_dev.dart';
+import 'package:shared/shared.dart';
 
 void main() {
+  // ignore: lines_longer_than_80_chars
   const apiRepository = ApiRepository();
-  bootstrap(() => const App(
-        apiRepository: apiRepository,
-      ));
+  bootstrap(
+    (powerSyncRepository) {
+      return const App(apiRepository: apiRepository);
+    },
+    appFlavor: AppFlavor.staging(),
+    firebaseOptions: DefaultFirebaseOptions.currentPlatform,
+  );
 }
+
