940df652b163f75f3cddb7fba9fa64b2da142d67
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2024-10-22 14:43:13 +0700

    add app snakebar

--- packages/app_ui/lib/src/widgets/widgets.dart
+++ packages/app_ui/lib/src/widgets/widgets.dart
@@ -6,7 +6,7 @@ export 'app_divider.dart';
 export 'app_loading_progress.dart';
 export 'app_logo.dart';
 export 'app_scaffold.dart';
-// export 'app_snackbar.dart';
+export 'app_snackbar.dart';
 export 'app_text_field.dart';
 export 'gap.dart';
 export 'tappable.dart';
