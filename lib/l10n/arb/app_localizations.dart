b09cb043e7478aa1bc93fdfc85b00d3c08a2c775
npbn42 <148618708+npbn42@users.noreply.github.com>
2024-10-22 23:06:14 +0700

    set up

--- lib/l10n/arb/app_localizations.dart
+++ lib/l10n/arb/app_localizations.dart
@@ -7,8 +7,6 @@ import 'package:intl/intl.dart' as intl;
 
 import 'app_localizations_en.dart';
 
-// ignore_for_file: type=lint
-
 /// Callers can lookup localized strings with an instance of AppLocalizations
 /// returned by `AppLocalizations.of(context)`.
 ///
