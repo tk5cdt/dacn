64830e2056e0d022e680ac115792191ad12f65ed
npbn42 <148618708+npbn42@users.noreply.github.com>
2024-11-20 01:23:20 +0700

    fix sql

--- packages/powersync_repository/lib/powersync_repository.dart
+++ packages/powersync_repository/lib/powersync_repository.dart
@@ -1,6 +1,8 @@
 /// A Very Good Project created by Very Good CLI.
 library;
 
+//export 'package:powersync/sqlite3.dart';
+
 export 'package:supabase_flutter/supabase_flutter.dart' hide User;
 
 export 'src/powersync_repository.dart';
